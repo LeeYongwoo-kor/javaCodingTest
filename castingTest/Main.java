a58164f10e7230e694af5c48d399939ec253190a
LeeYongwoo-kor <newlyo1205@naver.com>
2021-07-14 22:20:28 +0900

    add castingTest

---
+++ castingTest/Main.java
@@ -0,0 +1,25 @@
+package castingTest;
+
+public class Main {
+    public static void main(String[] args) {
+        String stringNumber = "18.5";
+        Long longNumber = 8L;
+        Long longNumber2 = 7L;
+
+        String result = Double.toString(Double.parseDouble(stringNumber) * longNumber2 / 10);
+        System.out.println(result);
+
+        Integer result2 = (int) (longNumber / 10);
+        Integer result3 = (int) (longNumber2 % 10);
+        System.out.println(result2);
+        System.out.println(result3);
+
+        String result4 = Double.toString(Math.floor(Double.parseDouble(stringNumber) * longNumber2) / 10.0);
+        System.out.println(result4);
+
+        String result5 = Double.toString(Math.floor(Double.parseDouble(stringNumber) * longNumber2) / 10.0);
+        String sum = Double.toString(Double.parseDouble(result4) + Double.parseDouble(result5));
+
+        System.out.println(sum);
+    }
+}
